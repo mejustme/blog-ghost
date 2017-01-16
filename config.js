6fe3aa29abac1581e6b07203136569a920b36121
陈钦辉 <hzchenqinhui@corp.netease.com>
2017-01-16 22:21:17 +0800

    @dev 使用域名

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://121.40.139.122/',
+        url: 'http://www.if-elseif-else/',
         mail: {},
         database: {
             client: 'sqlite3',
